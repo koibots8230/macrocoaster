a07e697ce70f7d9a7d674f1f72613afdb2063ac0
Toby Mickelson <tmickelson@tinyfern.com>
2024-01-28 17:36:55 -0500

    initial commit

---
+++ config.h
@@ -0,0 +1,28 @@
+// Copyright 2023 placeholder (@placeholder)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+#define ENCODERS_PAD_A { B4, D7 }
+#define ENCODERS_PAD_B { E6, C6 }
+#define ENCODER_RESOLUTION 4
+#define DIP_SWITCH_PINS { D4, B5 }
+
+#define BOOTMAGIC_LITE_ROW 1
+#define BOOTMAGIC_LITE_COLUMN 1
---
+++ keymaps/default/keymap.c
@@ -0,0 +1,44 @@
+
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include "keycodes.h"
+#include QMK_KEYBOARD_H
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        JS_1, JS_2, KC_NO,
+        JS_4, JS_5, JS_6,
+        JS_7, JS_8, JS_9,
+        JS_10, JS_11, JS_12
+    )
+};
+
+
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [0] =  {ENCODER_CCW_CW(JS_13, JS_14), ENCODER_CCW_CW(JS_16, JS_15)},
+};
+
+
+/*
+bool dip_switch_update_user(uint8_t index, bool active) {
+    switch (index) {
+        case 0:
+            if (active) {
+			    tap_code(JS_3);
+			    // JS_3;
+            }
+            break;
+        case 1:
+            if (active) {
+			    tap_code(JS_0);
+			    // JS_0;
+            }
+            break;
+    }
+
+    return true;
+}
+*/
+
